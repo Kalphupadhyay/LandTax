fe866d844b8fc1ffa9a767326232223bf49c8f48
kalph <kalpupadhyay@gmail.com>
2020-05-02 12:30:37 +0530

    Updates

---
+++ precache-manifest.fa6ca5093b9d4f473a82c4653df03554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bc1e771e54b4d6af040e2d5a43508f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8087699649accadf68f9",
+    "url": "/static/css/main.4159ce6c.chunk.css"
+  },
+  {
+    "revision": "f38daf89e8b3d11e7e09",
+    "url": "/static/js/2.0c0cb0f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0c0cb0f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8087699649accadf68f9",
+    "url": "/static/js/main.27a863c3.chunk.js"
+  },
+  {
+    "revision": "a9f7582f6e301f96fe15",
+    "url": "/static/js/runtime-main.596426fe.js"
+  },
+  {
+    "revision": "21299c6471005419b4b95905da6a9411",
+    "url": "/static/media/background.21299c64.png"
+  }
+]);
